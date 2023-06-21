70216a89f35b50b0abadd1d88f1f222bc3bb41fb
Farhan Hanif <madjavacoder@gmail.com>
2023-06-21 10:50:31 +0700

    added btn turn to red project

--- app/src/main/java/org/chillrend/DTSApplication/MainActivity.java
+++ app/src/main/java/org/chillrend/DTSApplication/MainActivity.java
@@ -1,5 +1,8 @@
 package org.chillrend.DTSApplication;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_ok = findViewById(R.id.button_to_red);
+        final Button red_btn = findViewById(R.id.button);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                red_btn.setBackgroundColor(Color.RED);
+            }
+        });
     }
 }
